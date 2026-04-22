345473b6d98b65de994722343479c9fbdb1fbfac
Vitor Taichi Taira <vitortaira@estudante.ufscar.br>
2026-04-22 18:33:17 -0300

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        string = []
+        for i in range(len(s)):
+            char = s[i].lower()
+            if char.isalnum():
+                string.append(char)
+
+        sizeString = len(string)
+        # for i in range((sizeString // 2)):
+        #     if string[i] != string[sizeString - i - 1]:
+        #         return False
+        listFinal = string[::-1]
+        if listFinal != string:
+            return False
+        return True
